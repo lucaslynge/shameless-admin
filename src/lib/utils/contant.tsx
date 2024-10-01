8dfb2d154806063cbc032200dfafa9d043e408c4
ameer_hamza0331 <hafizameerhamza87@gmail.com>
2024-10-01 19:15:46 +0500

    updated

--- src/lib/utils/contant.tsx
+++ src/lib/utils/contant.tsx
@@ -2,5 +2,4 @@
 // export const BASE_URL='https://370f-2400-adc5-10d-5800-a51b-b9e4-8b03-86b5.ngrok-free.app/'
 
 export const BASE_URL="https://shamelesspath-be-b5b22ff3a687.herokuapp.com/"
-
-// export const BASE_URL="https://dd52-2400-adc5-10d-5800-2c20-a752-4874-6e7.ngrok-free.app/"
+// export const BASE_URL="https://dab4-2400-adc5-16b-5900-f463-8773-77a4-8083.ngrok-free.app"
