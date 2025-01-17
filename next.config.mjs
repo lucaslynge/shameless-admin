9da49264d44b4c71b2e65fb5e14e4baed62b3006
lucaslynge <Hello@shamelesspath.com>
2025-01-17 09:46:09 +0800

    add md editor and removed domshell

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  transpilePackages: ["@mdxeditor/editor"],
   reactStrictMode: true,
-  images:{
-    domains:['res.cloudinary.com']
-  }
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+  webpack: (config) => {
+    // this will override the experiments
+    config.experiments = { ...config.experiments, topLevelAwait: true };
+    // this will just update topLevelAwait property of config.experiments
+    // config.experiments.topLevelAwait = true
+    return config;
+  },
 };
 
 export default nextConfig;
